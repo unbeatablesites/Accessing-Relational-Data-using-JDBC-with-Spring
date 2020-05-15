180ddc70fd419dbf74d684c05508b7301ea1c1a5
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-14 21:53:50 -0400

    Add dependencies

---
+++ JdbcWithH2/src/main/java/com/company/JdbcWithH2/JdbcWithH2Application.java
@@ -0,0 +1,13 @@
+package com.company.JdbcWithH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcWithH2Application.class, args);
+	}
+
+}
---
+++ JdbcWithH2/src/test/java/com/company/JdbcWithH2/JdbcWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.JdbcWithH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
